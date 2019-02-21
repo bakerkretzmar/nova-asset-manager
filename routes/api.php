5bcf7435748d6e51137bcb87d880114dd515e04b
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-02-21 15:55:32 -0500

    Wip

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,13 @@ use Bakerkretzmar\AssetManager\Http\Controllers\FolderController;
 
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('files')->group(function () {
+    Route::post('upload', [FileController::class, 'create']);
+    Route::post('delete', [FileController::class, 'destroy']);
+});
+
 Route::prefix('folders')->group(function () {
     Route::get('info', [FolderController::class, 'show']);
     Route::post('create', [FolderController::class, 'create']);
     Route::post('delete', [FolderController::class, 'destroy']);
-});
-
-Route::prefix('files')->group(function () {
-    Route::post('upload', [FileController::class, 'create']);
-    Route::post('delete', [FileController::class, 'destroy']);
 });
