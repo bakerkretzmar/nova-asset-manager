f8cfababe04965211bd78438cb6aecd05b876942
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-02-22 14:17:33 -0500

    Register global components

--- resources/js/field.js
+++ resources/js/field.js
@@ -2,4 +2,9 @@ Nova.booting((Vue, router) => {
     // Vue.component('index-asset-manager-field', require('./components/IndexField'))
     Vue.component('detail-asset-manager-field', require('./components/DetailField'))
     Vue.component('form-asset-manager-field', require('./components/FormField'))
+
+    Vue.component('asset-manager-back-button', require('./components/partials/BackButton'))
+    Vue.component('asset-manager-breadcrumbs', require('./components/partials/Breadcrumbs'))
+    Vue.component('asset-manager-view-buttons', require('./components/partials/ViewButtons'))
+    Vue.component('asset-manager', require('./components/Manager'))
 })
